46c922fd1832e89651cced0f81ded01b3fd57c9c
Munir <munir@anodyne.dk>
2021-04-20 15:51:17 +0200

    header and footer added to the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
   return (
   <>
+  <Header/>
   <h1>welcome to LineMeUp shop</h1>
+  <Footer/>
   </>
   );
 }
