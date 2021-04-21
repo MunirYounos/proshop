bbebb968515023e14d1168e815d325b80c9ec658
Munir <munir@anodyne.dk>
2021-04-21 15:53:21 +0200

    added routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import  { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
   return (
-  <>
-  <Header/>
-  <h1>welcome to LineMeUp shop</h1>
-  <Footer/>
-  </>
+  <Router>
+    <Header/>
+      <main className="py-3">
+        <Container>
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/product/:id' component={ProductScreen}  />
+      </Container>
+      </main>
+    <Footer/>
+  </Router>
   );
 }
 
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Container, Row, Col} from 'react-bootstrap'
 const Footer = () => {
 	return (
 		<footer>
-			foooter
+			<Container>
+				<Row>
+					<Col className="text-center py-3">
+						All rights reserved &copy; LineMeUp Shops.
+					</Col>
+				</Row>
+
+			</Container>
 		</footer>
 	)
 }
